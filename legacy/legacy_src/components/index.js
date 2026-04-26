ffd0a301ecf0d100505788d43abeb70885dd0af5
Zezhang Liu <zezhang.liu@bytedance.com>
2026-04-26 12:26:06 +0800

    chore: remove legacy source

--- legacy/legacy_src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Detail from './detail'
-import Searcher from './searcher'
-
-const components = {
-  Detail,
-  Searcher,
-}
-
-export default components
--- legacy/legacy_src/options_page.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/options_app'
-
-ReactDOM.render(<App />, document.getElementById('main'))
